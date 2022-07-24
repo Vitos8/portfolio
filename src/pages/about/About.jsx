036c7b26573db03366855859579984ef8254a285
Vitoslavanda <vitka.10165@gmail.com>
2022-07-24 23:00:09 +0300

    Added link to resume

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
                          <h3 className="text-blue font-bold text-lg mb-10">About me</h3>
                          <p className="w-[500px] mb-20 leading-6"> I am a junior front-end developer and i'm open to new opportunities. Very quick to learn, hardworking and confident, honest and responsible, attention to detail and
 great problem solving skills, positive attitude and energy , disciplined. For more details, open my resume below. </p>
-                         <Button color={'blue'} bg={'black'} children={'Download CV'} />
+                         <Button link={'https://drive.google.com/file/d/1W_5DiVJd1x33ffm1R7PCTlG4UhvaTOUo/view'} color={'blue'} bg={'black'} children={'Download CV'} />
                     </div>
                </div>
           </div>
